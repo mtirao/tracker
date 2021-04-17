1c90787db7662dc9cec11c54dae5fa8988a57f33
Marcos Tirao <marcos.tirao@naranja.com>
2021-04-17 20:19:09 -0300

    Update show issue view

--- Web/View/Issues/Show.hs
+++ Web/View/Issues/Show.hs
@@ -11,6 +11,44 @@ instance View ShowView where
                 <li class="breadcrumb-item active">Show Issue</li>
             </ol>
         </nav>
-        <h1>Show Issue</h1>
-        <p>{issue}</p>
+
+         <ul class="list-group mb-3">
+            <li class="list-group-item d-flex justify-content-between lh-condensed">
+              <div>
+                <h6 class="my-0">Mantis</h6>
+                <small class="text-muted">{(get #number issue)}</small>
+              </div>
+            </li>
+            <li class="list-group-item d-flex justify-content-between lh-condensed">
+              <div>
+                <h6 class="my-0">Summary</h6>
+                <small class="text-muted">{(get #summary issue)}</small>
+              </div>
+            </li>
+            <li class="list-group-item d-flex justify-content-between lh-condensed">
+              <div>
+                <h6 class="my-0">Estimate effort (in days)</h6>
+                <small class="text-muted">{(get #days issue)}</small>
+              </div>
+            </li>
+            <li class="list-group-item d-flex justify-content-between lh-condensed">
+              <div>
+                <h6 class="my-0">Status</h6>
+                <small class="text-muted">{(get #status issue)}</small>
+              </div>
+            </li>
+            <li class="list-group-item d-flex justify-content-between lh-condensed">
+              <div>
+                <h6 class="my-0">Asignee</h6>
+                <small class="text-muted">{(get #assignee issue)}</small>
+              </div>
+            </li>
+            <li class="list-group-item d-flex justify-content-between lh-condensed">
+              <div>
+                <h6 class="my-0">Description</h6>
+                <small class="text-muted">{(get #description issue)}</small>
+              </div>
+            </li>
+          </ul>
+
     |]
